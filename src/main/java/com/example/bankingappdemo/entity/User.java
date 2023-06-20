1523f5e27c6531f31c76fa5c57e1c939b81f8995
Peace Obute <peaceobute65@gmail.com>
2023-06-20 20:00:10 +0100

    an update to the project

--- src/main/java/com/example/bankingappdemo/entity/User.java
+++ src/main/java/com/example/bankingappdemo/entity/User.java
@@ -21,43 +21,23 @@ public class User extends BaseEntity{
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(name = "first_name")
     private String firstName;
-
-    @Column(name = "last_name")
     private String lastName;
-
-    @Column(name = "other_name")
     private String otherName;
 
     @Enumerated(EnumType.STRING)
     private Gender gender;
-
-    @Column(name = "address")
     private String address;
-
-    @Column(name = "state-of-origin")
     private String stateOfOrigin;
-
-    @Column(name = "account-number")
     private String accountNumber;
-
-    @Column(name = "account-balance")
     private BigDecimal accountBalance;
 
     @Column(name = "email", unique = true, nullable = false)
     private String email;
-
-    @Column(name = "phoneNumber")
     private String phoneNumber;
-
-    @Column(name = "alt-phoneNumber")
     private String alternativePhoneNumber;
-
     @Enumerated(EnumType.STRING)
     private Status status;
-
     @Column(name = "encrypted-password", nullable = false, unique = true)
     private String encryptedPassword;
 }
